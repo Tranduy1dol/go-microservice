40ed5dbc09ada9c2bc9b6a9b36d272a0c5c0306e
Tranduy1dol <tmd.id2004@gmail.com>
2025-05-17 14:54:56 +0000

    feat: db migration & init

---
+++ cmd/migrate/migrations/000001_create_users.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email citext NOT NULL UNIQUE,
+    password bytea NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+);
