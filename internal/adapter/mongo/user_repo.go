a1c571958efca94cce183f79cfcf32c88057eca4
Tranduy1dol <tmd.id2004@gmail.com>
2026-04-30 20:03:40 +0700

    feat: user repository

---
+++ internal/adapter/mongo/user_repo.go
@@ -0,0 +1,81 @@
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"github.com/Tranduy1dol/learning-japanese/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type UserRepository struct {
+	collection *mongo.Collection
+}
+
+func NewUserRepository(db *mongo.Database) *UserRepository {
+	return &UserRepository{collection: db.Collection("users")}
+}
+
+func (r *UserRepository) GetByID(ctx context.Context, id string) (*domain.User, error) {
+	filter := bson.M{"_id": id}
+
+	var user domain.User
+	err := r.collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) GetByGoogleID(ctx context.Context, googleID string) (*domain.User, error) {
+	filter := bson.M{"google_id": googleID}
+
+	var user domain.User
+	err := r.collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) Upsert(ctx context.Context, googleID, email, name, pictureURL string) (*domain.User, error) {
+	filter := bson.M{"google_id": googleID}
+
+	update := bson.M{
+		"$set": bson.M{
+			"email":       email,
+			"name":        name,
+			"picture_url": pictureURL,
+			"updated_at":  time.Now(),
+		},
+		"$setOnInsert": bson.M{
+			"google_id":  googleID,
+			"role":       "user",
+			"created_at": time.Now(),
+			"study_progress": domain.Progress{
+				JLPTLevel: 5,
+			},
+		},
+	}
+
+	opts := options.FindOneAndUpdate().SetUpsert(true).SetReturnDocument(options.After)
+
+	var user domain.User
+	err := r.collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) UpdateProgress(ctx context.Context, userID string, progress domain.Progress) error {
+	filter := bson.M{"_id": userID}
+	update := bson.M{"$set": bson.M{"progress": progress}}
+	_, err := r.collection.UpdateOne(ctx, filter, update)
+	return err
+}
--- internal/port/user.go
+++ internal/port/user.go
@@ -9,6 +9,6 @@ import (
 type UserRepository interface {
 	GetByID(ctx context.Context, id string) (*domain.User, error)
 	GetByGoogleID(ctx context.Context, googleID string) (*domain.User, error)
-	Upsert(ctx context.Context, user *domain.User) error
+	Upsert(ctx context.Context, googleID, email, name, pictureURL string) (*domain.User, error)
 	UpdateProgress(ctx context.Context, userID string, progress domain.Progress) error
 }
