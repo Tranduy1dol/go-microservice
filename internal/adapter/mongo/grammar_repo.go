4f5f1ef2a911a9329acfd6dbdccfe8d9e93e8086
Tranduy1dol <tmd.id2004@gmail.com>
2026-04-30 17:24:56 +0700

    feat: grammar repository

---
+++ internal/adapter/mongo/grammar_repo.go
@@ -0,0 +1,102 @@
+package mongo
+
+import (
+	"context"
+
+	"github.com/Tranduy1dol/learning-japanese/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type GrammarRepository struct {
+	collection *mongo.Collection
+}
+
+func NewGrammarRepository(db *mongo.Database) *GrammarRepository {
+	return &GrammarRepository{collection: db.Collection("grammar")}
+}
+
+func (r *GrammarRepository) GetByID(ctx context.Context, id string) (*domain.Grammar, error) {
+	filter := bson.M{"_id": id}
+
+	var grammar domain.Grammar
+	err := r.collection.FindOne(ctx, filter).Decode(&grammar)
+	if err != nil {
+		return nil, err
+	}
+
+	return &grammar, nil
+}
+
+func (r *GrammarRepository) GetByJLPT(ctx context.Context, level int, limit int) ([]*domain.Grammar, error) {
+	filter := bson.M{"jlpt": level}
+
+	opt := options.Find().SetLimit(int64(limit))
+	cursor, err := r.collection.Find(ctx, filter, opt)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = cursor.Close(ctx) }()
+
+	var grammars []*domain.Grammar
+	if err := cursor.All(ctx, &grammars); err != nil {
+		return nil, err
+	}
+
+	return grammars, nil
+}
+
+func (r *GrammarRepository) GetRandom(ctx context.Context, count int) ([]*domain.Grammar, error) {
+	filter := bson.M{}
+	pipeline := mongo.Pipeline{
+		{{Key: "$match", Value: filter}},
+		{{Key: "$sample", Value: bson.M{"size": count}}},
+	}
+
+	cursor, err := r.collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = cursor.Close(ctx) }()
+
+	var grammars []*domain.Grammar
+	if err := cursor.All(ctx, &grammars); err != nil {
+		return nil, err
+	}
+
+	return grammars, nil
+}
+
+func (r *GrammarRepository) Create(ctx context.Context, g *domain.Grammar) error {
+	_, err := r.collection.InsertOne(ctx, g)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *GrammarRepository) BulkCreate(ctx context.Context, grammars []*domain.Grammar) (int64, error) {
+	docs := make([]any, len(grammars))
+	for i, q := range grammars {
+		docs[i] = q
+	}
+
+	result, err := r.collection.InsertMany(ctx, docs)
+	if err != nil {
+		return 0, err
+	}
+
+	return int64(len(result.InsertedIDs)), nil
+}
+
+func (r *GrammarRepository) Delete(ctx context.Context, id string) error {
+	filter := bson.M{"_id": id}
+	_, err := r.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/port/grammar.go
@@ -0,0 +1,16 @@
+package port
+
+import (
+	"context"
+
+	"github.com/Tranduy1dol/learning-japanese/internal/domain"
+)
+
+type GrammarRepository interface {
+	GetByID(ctx context.Context, id string) (*domain.Grammar, error)
+	GetByJLPT(ctx context.Context, level int, limit int) ([]*domain.Grammar, error)
+	GetRandom(ctx context.Context, count int) ([]*domain.Grammar, error)
+	Create(ctx context.Context, g *domain.Grammar) error
+	BulkCreate(ctx context.Context, grammars []*domain.Grammar) (int64, error)
+	Delete(ctx context.Context, id string) error
+}
